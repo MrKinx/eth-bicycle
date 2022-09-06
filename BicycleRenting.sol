739e9272c5468e5e5fb936f29a6e7fdf4651a411
MrKinx <mrkinx@outlook.com>
2022-09-06 11:03:35 +0300

    Add session log

--- BicycleRenting.sol
+++ BicycleRenting.sol
@@ -5,15 +5,19 @@ pragma solidity ^0.8.0;
 contract BicycleRenting {
     
     address owner;
-    uint fee;
+   // uint fee;
 
-    constructor(uint setFee) {
+    constructor() {
         owner = msg.sender;
-        fee = setFee;
-
     }
 
 // Add Renter informations
+struct Session{
+    uint sessionId;
+    uint sessionStart;
+    uint sessionEnd;
+    uint sessionDue;
+}
 
 struct Renter {
     string name;
@@ -24,31 +28,35 @@ struct Renter {
     bool active; 
     uint balance; 
     uint due;
+    uint sessionNum;
 }
 
 mapping (address => Renter) public renters;
+// mapping (uint => Session) public sessionDetails;
+mapping (address => mapping (uint => Session)) public sessions;
 
-function addRenter(string memory name, string memory lastName,  address payable walletAddress,  uint256 start,  uint256 end,  bool canRent,  bool active,  uint balance,  uint due) public {
-    renters[walletAddress] = Renter(name, lastName,walletAddress, start, end, active, balance, due);
+function addRenter(string memory name, string memory lastName,  address payable walletAddress, uint balance) public {
+    renters[walletAddress] = Renter(name, lastName,walletAddress, 0, 0, false, balance, 0, 0);
 }
 
-// Renting request (Check in)
-
-function checkInBike(address walletAddress) public {
-    require(renters[walletAddress].active == false);
+// Start Rent
+function reqBikeRide(address walletAddress) public {
+    require(canRentBike(walletAddress) == true, "You already have an active session");
     renters[walletAddress].start = block.timestamp;
     renters[walletAddress].active = true;
-
+    renters[walletAddress].sessionNum ++;
+    sessions[walletAddress][renters[walletAddress].sessionNum].sessionId = renters[walletAddress].sessionNum;
 }
 
-// Ending renting (Check out)
+// End Rent
 
-function checkOutBike(address walletAddress) public {
-    require(renters[walletAddress].active == true);
+function endBikeRide(address walletAddress) public {
+    require(renters[walletAddress].active == true, "You don't have an active session!");
     renters[walletAddress].end = block.timestamp;
     renters[walletAddress].active = false;
     setDue(walletAddress);
-
+    addSession(walletAddress, renters[walletAddress].sessionNum);
+    makePayment(walletAddress);
 }
 
 // Get session duration
@@ -59,14 +67,18 @@ function getTimeDif (uint end, uint start) internal pure returns(uint) {
 function getSessionDuration (address walletAddress) public view returns(uint){
     require(renters[walletAddress].active == false, "You have an ongoing session!");
     uint thisSession = getTimeDif(renters[walletAddress].end, renters[walletAddress].start);
-    uint sessionInMinutes = thisSession/60;
-    return sessionInMinutes;
+    return thisSession;
 }
 
 // Get Contract balance
 function contractBalance() public view returns(uint){
-    require(msg.sender == owner, "Only owner can execute this function");
+    require(msg.sender == owner, "Only owner can execute this function!");
     return address(this).balance;
+}
+
+// Add Money to balance
+function deposit(address walletAddress) public payable {
+    renters[walletAddress].balance += msg.value;
 }
 
 // Get Renter's balance
@@ -79,12 +91,33 @@ function setDue(address walletAddress) internal{
     uint sessionTime = getSessionDuration(walletAddress);
     renters[walletAddress].due = sessionTime * 5000000000000000;
 }
-function deductDue(address walletAddress) internal{
-    renters[walletAddress].balance = renters[walletAddress].balance -  renters[walletAddress].due;
+
+// Set Payment
+function makePayment(address walletAddress) public payable {
+    require(renters[walletAddress].due > 0, "No active payment were found!");
+    require(renters[walletAddress].balance >= msg.value, "Not enough balance!");
+    renters[walletAddress].balance -= renters[walletAddress].due;
+    renters[walletAddress].due = 0;
+    renters[walletAddress].start = 0;
+    renters[walletAddress].end = 0;
 }
 
+//Check rent request order
 function canRentBike(address walletAddress) public view returns(bool){
-    return (!renters[walletAddress].active);
+    if(renters[walletAddress].active == false && renters[walletAddress].due == 0){
+        return (true);
+    }
+    else{
+        return(false);
+    }
+
+}
+
+//Add Session Record
+function addSession(address walletAddress, uint sessionId) internal{
+    sessions[walletAddress][sessionId].sessionStart = renters[walletAddress].start;
+    sessions[walletAddress][sessionId].sessionEnd = renters[walletAddress].end;
+    sessions[walletAddress][sessionId].sessionDue = renters[walletAddress].due;
 }
 
 }
